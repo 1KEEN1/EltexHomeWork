2bcef278b28a5004ad0727d224b4c28c6e490376
Ruslan Pirakov <rpirakov20@gmail.com>
2025-12-22 18:21:53 +0700

    Add second task.

---
+++ HomeWork_2/main.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+
+#define N 3
+#define M 10
+#define X 5
+
+void array() {
+  int count = 1;
+  printf("Вывод квадратной матрицы размера на N=3\n");
+  int array[N][N];
+  //   заполнение матрицы
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      array[i][j] = count;
+      count++;
+    }
+  }
+  //   вывод матрицы
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      printf("%d ", array[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+
+void reverse() {
+  printf("Вывод массива в обратном порядке размера на M=10\n");
+  int array[M];
+  for (int i = 0; i < M; i++) {
+    array[i] = i + 1;
+  }
+  for (int i = 0; i < M; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+  int buff;
+  for (int i = 0; i < M / 2; i++) {
+    buff = array[i];
+    array[i] = M - i;
+    array[M - 1 - i] = buff;
+  }
+  for (int i = 0; i < M; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
+
+
+void triangle() {
+  int array[M][M];
+  int count;
+  printf("Заполняет верхний треугольник матрицы 1, а нижний 0. Размер матрицы "
+         "M=10\n");
+  for (int i = 0; i < M; i++) {
+    count = 0;
+    for (int j = 0; j < M; j++) {
+      if (count < M - i - 1) {
+        array[i][j] = 0;
+        count++;
+      } else {
+        array[i][j] = 1;
+      }
+    }
+  }
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < M; j++) {
+      printf("%d ", array[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+
+void printSpiral() {
+  printf("Заполнение матрицы числами улиткой.\n");
+  int array[X][X];
+  int value = 1;
+  int minCol = 0;
+  int maxCol = X - 1;
+  int minRow = 0;
+  int maxRow = X - 1;
+  while (value <= X * X) {
+    for (int i = minCol; i <= maxCol; ++i) {
+      array[minRow][i] = value;
+      value++;
+    }
+    minRow++;
+
+    for (int i = minRow; i <= maxRow; ++i) {
+      array[i][maxCol] = value;
+      value++;
+    }
+    maxCol--;
+
+    for (int i = maxCol; i >= minCol; --i) {
+      array[maxRow][i] = value;
+      value++;
+    }
+    maxRow--;
+
+    for (int i = maxRow; i >= minRow; --i) {
+      array[i][minCol] = value;
+      value++;
+    }
+    minCol++;
+  }
+  for (int i = 0; i < X; i++) {
+    for (int j = 0; j < X; j++) {
+      printf("%d\t", array[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+int main() {
+  char a;
+  printf("1. Вывести квадратную матрицу по заданному N.\n");
+  printf("2. Вывести заданный массив размером N в обратном порядке.\n");
+  printf("3. Заполнить верхний треугольник матрицы а нижний 0.\n");
+  printf("4. Заполнить матрицу числами от 1 до N^2 уликой\n");
+  printf("-------------------------\n");
+  printf("Напишите цифру соответствующего задания\n");
+
+  scanf("%c", &a);
+
+  switch (a) {
+  case '1':
+    printf("\n\n");
+    array();
+    break;
+  case '2':
+    printf("\n\n");
+    reverse();
+    break;
+  case '3':
+    printf("\n\n");
+    triangle();
+    break;
+  case '4':
+    printf("\n\n");
+    printSpiral();
+    break;
+
+  default:
+    break;
+  }
+  return 0;
+}
