9054a5a9e84fa9088a3b7e7ac60d8bc935f526e2
Pirakov Ruslan <rpirakov20@gmail.com>
2026-01-12 20:52:45 +0700

    Update main.c

--- HomeWork_3/main.c
+++ HomeWork_3/main.c
@@ -22,7 +22,7 @@ void correctPointer() {
   printf("x = %f, ", x);
   float y = 6.0;
   printf("y = %f\n", y);
-  float *xp = &y; // TODO: отредактируйте эту строку, и только правую часть
+  float *xp = &y;
   float *yp = &y;
   printf("Результат: %f\n", *xp + *yp);
 }
